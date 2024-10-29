b09e139fdc8bd6b6afa91ab7aeea9c45c199f9c8
albertoancarani <alberto.ancarani4@studio.unibo.it>
2024-10-29 16:49:05 +0100

    Delete perform_optimization.m

--- perform_optimization.m
+++
@@ -1,9 +0,0 @@
-function perform_optimization()
-global OPT
-
-switch OPT.options.optimizer
-    case 'fmincon-active-set'
-        OPT.history = runfmincon(OPT.dv,@(x)obj(x),@(x)nonlcon(x));
-    case 'mma'
-        OPT.history = runmma(OPT.dv,@(x)obj(x),@(x)nonlcon(x));
-end
